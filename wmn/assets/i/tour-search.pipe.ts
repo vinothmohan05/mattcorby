1b93578c8597d007291dc7ff59d780d33adbcd9d
Your Name <officialmailforkarthick@gmail.com>
2017-06-20 19:21:05 +0530

    initial commit

---
+++ wmn/assets/i/tour-search.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'tourSearch'
+})
+export class TourSearch implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    console.log("this is args");
+    console.log(args);
+    let filter = args.toLocaleLowerCase();
+    return filter ? value.filter(t=> t.venue_city.toLocaleLowerCase().indexOf(filter) != -1) : value;
+    //return value;
+
+  }
+
+}
