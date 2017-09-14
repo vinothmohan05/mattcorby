488388a37ab07a03616f00cdd14a83f4da6aa590
manojkumar21 <ManojKumar.Elangovan@wmg.com>
2017-09-14 16:45:55 +0530

    4.7.8

--- wmn/assets/i/tour-search.pipe.ts
+++ wmn/assets/i/tour-search.pipe.ts
@@ -6,8 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TourSearch implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    console.log("this is args");
-    console.log(args);
+   
     let filter = args.toLocaleLowerCase();
     return filter ? value.filter(t=> t.venue_city.toLocaleLowerCase().indexOf(filter) != -1) : value;
     //return value;
